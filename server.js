149a885b8ccf6a894dccf110abcb7a2249a1d76c
Hans ter Horst <hbhorst@mac.com>
2019-01-31 17:50:39 +0100

    Create User test route

--- server.js
+++ server.js
@@ -2,6 +2,9 @@ const express = require('express');
 const mongoose = require('mongoose');
 const mongoUri = require('./config/keys_dev').mongoUri;
 
+// routes
+const user = require('./routes/user');
+
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -13,5 +16,8 @@ mongoose
 app.get('/', (req, res) => {
   res.send('Hello server');
 });
+
+// Use routes
+app.use('/api/user', user);
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
