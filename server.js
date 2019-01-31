ba0d973d59343dc35418c345f94cfd8ba9a49505
Hans ter Horst <hbhorst@mac.com>
2019-01-31 17:21:09 +0100

    Setup mongoDB

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const mongoUri = require('./config/keys_dev').mongoUri;
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+mongoose
+  .connect(mongoUri)
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.log(err));
 
 app.get('/', (req, res) => {
   res.send('Hello server');
