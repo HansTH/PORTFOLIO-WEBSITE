a2bfcf71dbd70a8a133749acc6c6af462d312553
Hans ter Horst <hbhorst@mac.com>
2019-02-03 20:04:56 +0100

    Create GET request for login user

--- routes/user.js
+++ routes/user.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const key = require('../config/keys');
 
 // Load user model
 const User = require('../models/User');
@@ -49,6 +51,44 @@ router.post('/register', (req, res) => {
         });
       });
     }
+  });
+});
+
+// @route   GET /api/user/login
+// @desc    Login user, return a JWT token
+// @access  Public
+router.get('/login', (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+
+  User.findOne({ email }).then(user => {
+    // check user excist
+    if (!user) {
+      return res.status(404).json({ email: 'User not found' });
+    }
+
+    // check password
+    bcrypt.compare(password, user.password).then(isMatch => {
+      if (isMatch) {
+        const payload = { id: user.id, name: user.name, avatar: user.avatar };
+
+        // sign token
+
+        jwt.sign(
+          payload,
+          key.secretOrKey,
+          { expiresIn: 3600 },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: `Bearer ${token}`
+            });
+          }
+        );
+      } else {
+        return res.status(400).json({ password: 'Password incorrect' });
+      }
+    });
   });
 });
 
