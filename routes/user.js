f8d6866f6b6f4f047ba530dc31643978839fe96c
Hans ter Horst <hbhorst@mac.com>
2019-02-03 14:48:05 +0100

    Create POST request for register a user

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,10 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
+
+// Load user model
+const User = require('../models/User');
 
 // @route   GET /api/user/test
 // @desc    Test user route
@@ -7,6 +12,43 @@ const router = express.Router();
 router.get('/test', (req, res) => {
   res.json({
     message: 'User works'
+  });
+});
+
+// @route   POST /api/user/register
+// @desc    Register user
+// @access  Public
+router.post('/register', (req, res) => {
+  User.findOne({ email: req.body.email }).then(user => {
+    if (user) {
+      return res.status(400).json({ email: 'Email already exist' });
+    } else {
+      // create a avatar image
+      const avatar = gravatar.url(req.body.avatar, {
+        s: '200', // size
+        r: 'pg', // rating
+        d: 'mm' // default icon
+      });
+
+      const newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        avatar: avatar
+      });
+
+      // create a crypted password
+      bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+          if (err) throw err;
+          newUser.password = hash;
+          newUser
+            .save()
+            .then(user => res.json(user))
+            .catch(err => console.log(err));
+        });
+      });
+    }
   });
 });
 
