779df0f7119caa04e56ccb2761af7e0619dad9d2
Hans ter Horst <hbhorst@mac.com>
2019-03-02 19:41:26 +0100

    Create Loading component.

---
+++ client/src/components/common/Loading.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+class Loading extends Component {
+  state = {
+    text: this.props.text
+  };
+
+  componentDidMount() {
+    const { text, speed } = this.props;
+    const stopper = text + '...';
+
+    this.interval = window.setInterval(() => {
+      this.state.text === stopper
+        ? this.setState(() => ({
+            text: text
+          }))
+        : this.setState(prevState => ({
+            text: prevState.text + '.'
+          }));
+    }, speed);
+  }
+
+  render() {
+    return (
+      <div>
+        <h2 className='text-color text-center my-2'>{this.state.text}</h2>
+      </div>
+    );
+  }
+}
+
+Loading.propTypes = {
+  text: PropTypes.string.isRequired,
+  speed: PropTypes.number.isRequired
+};
+
+Loading.defaultProps = {
+  text: 'Loading',
+  speed: 400
+};
+
+export default Loading;
