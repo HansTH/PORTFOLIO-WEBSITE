e15ee807d6de8c799e69bfadf60b514f1fa285ce
Hans ter Horst <hbhorst@mac.com>
2019-03-11 09:43:28 +0100

    Prepare for deploy.

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const mongoUri = require('./config/keys').mongoUri;
 const bodyParser = require('body-parser');
 const passport = require('passport');
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -27,5 +28,15 @@ mongoose
 // Use routes
 app.use('/api/user', user);
 app.use('/api/profile', profile);
+
+// Server static assets if in production
+if (process.env.NODE_ENV === 'production') {
+  // set static folder
+  app.use(express.static('client/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
